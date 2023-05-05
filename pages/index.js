3b09caf4e08f465e61f458357f59a8cc98eaecdd
el X <alexanderzimbelmann4@gmail.com>
2023-05-05 11:24:38 +0200

    add spotlist

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 import SpotCard from "../components/SpotCard.js";
-
+import SpotList from "../components/SpotList.js";
 export default function App() {
   return (
     <div>
       <h1>TheCaliSpots</h1>
-      <SpotCard name={"Alex"} adress={"asdfsgsd"} />
+      <SpotCard />
+      <SpotList />
     </div>
   );
 }
