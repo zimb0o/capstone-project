1b8eaaee25506a29bc70a56657924b52c3ae7cef
el X <alexanderzimbelmann4@gmail.com>
2023-05-10 10:12:59 +0200

    resolve merge conflict

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,8 @@
-import React from "react";
-import styled from "styled-components";
-import SpotCard from "../components/SpotCard.js";
-<<<<<<< HEAD
-import SpotList from "@/components/SpotList.js";
-=======
 import SpotList from "../components/SpotList.js";
-import Header from "../components/Header.js";
->>>>>>> b6c7bb33c60b1395df7f3d428729e125d4310b81
 
 export default function App() {
   return (
     <div>
-<<<<<<< HEAD
-      <h1>TheCaliSpots</h1>
-=======
-      <Header />
->>>>>>> b6c7bb33c60b1395df7f3d428729e125d4310b81
-      <SpotCard />
       <SpotList />
     </div>
   );
