7d917742301a716159ed6d49b64c2926674c858a
el X <alexanderzimbelmann4@gmail.com>
2023-05-10 14:44:55 +0200

    add header

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import SpotList from "../components/SpotList.js";
 import Bottom from "../components/Bottom.js";
+import Header from "../components/Header.js";
 export default function Home() {
   return (
     <div>
+      <Header />
       <SpotList />
       <Bottom />
     </div>
