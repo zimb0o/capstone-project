fbc97d96ce59c2e8c57e1724e461f92ebe44e4a6
el X <alexanderzimbelmann4@gmail.com>
2023-05-19 13:04:10 +0200

    delete not neccessary code

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,7 @@
-import { useState } from "react";
 import GlobalStyle from "../styles";
-import { LocalStorageState } from "use-local-storage-state";
+
 import { mockSpots } from "../helpers/mockSpots.js";
 import useLocalStorage from "@/helpers/localStorage";
-import Layout from "../components/Layout.js";
 export default function App({ Component, pageProps }) {
   const [spots, setSpots] = useLocalStorage("spots", mockSpots);
   const addSpotHandler = (newSpot) => {
